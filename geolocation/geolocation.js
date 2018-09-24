0f2f1ed1bf3784ae822efbfbf5134fb69c065551
jennifermorgan2016 <jennifer.morgan@ise.ie>
2018-09-24 11:50:56 +0100

    Created helper module

--- geolocation/geolocation.js
+++ geolocation/geolocation.js
@@ -1,16 +1,19 @@
 const request = require('request');
+
   
 const G_API=JSON.parse(require('fs').readFileSync('secrets.json','utf8'))['google-maps-api-key'];
 
-var drawLine= (n) => {
-  console.log('-'.repeat(n));
-};
-var verbose = (obj) => {
-  drawLine(100);
-  console.log(JSON.stringify(obj, undefined, 2));
-  drawLine(100);
-};
 
+var extractAdressDetails = (results) => {
+  let lng=results.geometry.location.lng;
+  let lat=results.geometry.location.lat;
+  let address=results.formatted_address;
+  return {
+    lat,
+    lng,
+    address
+  };
+};
 var getAddress = (address, callback) => {
   let url=`https://maps.googleapis.com/maps/api/geocode/json?key=${G_API}`;
   let encodedAddress=encodeURIComponent(address);
@@ -25,14 +28,7 @@ var getAddress = (address, callback) => {
     if(error) {
       console.log('There was an error connecting to the API');
     } else if(body.status === "OK") {
-      let lng=body.results[0].geometry.location.lng;
-      let lat=body.results[0].geometry.location.lat;
-      let address=body.results[0].formatted_address;
-      callback({
-        lat,
-        lng,
-        address
-      });
+      callback( extractAdressDetails(body.results[0]));
     } else {
       console.log('Address not found');
     }
---
+++ helpers/screen.js
@@ -0,0 +1,13 @@
+var drawLine= (n) => {
+  console.log('-'.repeat(n));
+};
+var verbose = (obj) => {
+  drawLine(100);
+  console.log(JSON.stringify(obj, undefined, 2));
+  drawLine(100);
+};
+
+module.exports = {
+  drawLine,
+  verbose
+};
